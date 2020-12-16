17f37672bec8d685b5b70123e3eba3b4140a2e6b
Kh, Sura | Sura | LPD <sura.kh@rakuten.com>
2020-12-16 11:03:08 +0900

    add pages

---
+++ src/views/home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Home extends Vue {}
+</script>
