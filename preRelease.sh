cb3599f08cdf59e7f2c44f1cfed8a46d4cfd7ffd
sura.kh <sura.kh@rakuten.com>
2023-10-15 21:36:00 +0900

    fix: bug

---
+++ preRelease.sh
@@ -0,0 +1,35 @@
+RED='\033[0;31m'
+NO_COLOR='\033[0m'
+
+echo -e "-----------------------------------------"
+echo "[Release Process] Install packages"
+echo -e "-----------------------------------------"
+yarn install || exit 1
+echo -e "\n"
+
+echo -e "-----------------------------------------"
+echo "[Release Process] Build"
+echo -e "-----------------------------------------"
+yarn build || exit 1
+echo -e "\n"
+
+echo -e "-----------------------------------------"
+echo "[Release Process] Pack"
+echo -e "-----------------------------------------"
+yarn rimraf artifacts
+mkdir artifacts
+yarn pack:crx
+yarn pack:zip
+mv key.pem ./artifacts/
+echo -e "\n"
+
+echo -e "-----------------------------------------"
+echo "[Release Process] Check diff"
+echo -e "-----------------------------------------"
+if git diff --quiet; then
+  echo "No changes found."
+else
+  echo -e "${RED}Changes found. Exiting...${NO_COLOR}"
+  exit 1;
+fi
+echo -e "\n"
